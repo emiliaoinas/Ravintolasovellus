687d06297b0f7f3154a67b13ced64a29deae4c7f
emiliaoinas <emilia.oinas@helsinki.fi>
2024-11-26 15:34:14 +0200

    Add table for admins

--- schema.sql
+++ schema.sql
@@ -2,7 +2,13 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     username TEXT UNIQUE, 
     password TEXT,
-    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE admins (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    application TEXT,
+    start_time TIMESTAMP
 );
 
 CREATE TABLE restaurants (
